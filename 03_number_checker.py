56fb1b8b394982cee8d987f82e12a00ca4cfe234
ChrisGreig <greig.chris@awatapu.school.nz>
2022-05-18 13:20:04 +1200

    made number checker
    Signed-off-by: ChrisGreig <greig.chris@awatapu.school.nz>

--- 03_number_checker.py
+++ 03_number_checker.py
@@ -1,21 +1,24 @@
 # Functions go here
+def num_check(question, low, high):
+    error = "Please enter a whole number between 1 and 10\n"
 
+    valid = False
+    while not valid:
+       try:
+            # ask the question
+            response = int(input(question))
 
-# Main routine goes here
+            # if the amount is too low / too high give
+            if  low < response <= high:
+                return response
+            # output an error
+            else:
+                print(error)
+       except ValueError:
+                print(error)
 
-error = "Please enter a whole number between 1 and 10\n"
+# main routine
 
-valid = False
-while not valid:
-    try:
-        # ask the question
-        response = int(input("How much would you "
-                         "like to play with? "))
-        # if the amount is too low / too high give
-        if 0 < response <= 10:
-            print("You have asked to play "
-                  "with ${}".format(response))
+how_much = num_check("Pick a number",1,10)
 
-        # output an error
-        else:
-            print(error)
+print("You will be spending ${}".format(how_much))
